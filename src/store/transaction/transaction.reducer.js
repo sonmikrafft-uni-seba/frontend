a74392b09360d6a7c99ac1c7e0277fd10509c6d2
Sonja Krafft <ga83bin@mytum.de>
2022-06-30 01:14:03 +0200

    save states of all transactions

--- src/store/transaction/transaction.reducer.js
+++ src/store/transaction/transaction.reducer.js
@@ -1,19 +1,19 @@
 import { ACTION_TYPES } from './transaction.actions.js';
 
-const initialState = { transaction: null, error: null };
+const initialState = { transactions: [], error: null };
 
 export const transaction = (state = initialState, action) => {
   switch (action.type) {
     case ACTION_TYPES.TRANSACTION_CREATE_SUCCESS:
       return {
         ...state,
-        transaction: action.payload,
+        transactions: [...state.transactions, action.payload],
         error: null,
       };
     case ACTION_TYPES.TRANSACTION_CREATE_FAIL:
       return {
         ...state,
-        transaction: null,
+        transactions: null,
         error: action.payload,
       };
     default:
