626311333024283e8755e26b102d15edfbbbd876
Tobias Vitt <tobias.vitt@tum.de>
2022-07-05 00:23:32 +0200

    Added banking redux to root

--- src/store/root.actions.js
+++ src/store/root.actions.js
@@ -2,3 +2,4 @@ export * from './user/user.actions.js';
 export * from './auth/auth.actions.js';
 export * from './transaction/transaction.actions.js';
 export * from './popup/popup.actions.js';
+export * from './banking/banking.actions.js';
